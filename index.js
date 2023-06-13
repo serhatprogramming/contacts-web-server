e35053f2688fede95bc1f7a18e6fdcf94f6f5928
serhatprogramming <serhatprogramming@gmail.com>
2023-06-13 10:52:07 -0400

    get info added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+// Hardcoded contacts data
+let contacts = [
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Smith", email: "jane@example.com" },
+  { id: 3, name: "Bob Johnson", email: "bob@example.com" },
+];
+
+// Route handler for "/api/contacts"
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+app.get("/api/info", (req, res) => {
+  const numContacts = contacts.length;
+  const appName = "Contacts Web Server";
+
+  const response = `<h1>${appName}</h1>
+    <p>Number of contacts: ${numContacts}</p>`;
+
+  res.send(response);
+});
+
+app.get("/api/contacts/:id", (req, res) => {
+  const contactId = Number(req.params.id);
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).json({ error: "Contact not found" });
+  }
+});
+
+// Start the server
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("Server is running on port 3001");
+});
